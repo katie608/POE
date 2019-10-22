2f82f8018e4a8cd6792a9e97a07bca90aa6a426b
Katherine Foster <kfoster@olin.edu>
2019-10-22 18:43:13 -0400

    Make serial monitor conitnuously print which button was pressed while it is pressed

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -13,57 +13,23 @@ void setup() {
   pinMode(2, INPUT);
   pinMode(3, INPUT);
   pinMode(4, INPUT);
-  attachInterrupt(digitalPinToInterrupt(2), I2, RISING);
-  attachInterrupt(digitalPinToInterrupt(3), I3, RISING);
-  attachInterrupt(digitalPinToInterrupt(4), I4, RISING);
-}
-
-void I2() {
-  onButtonPress(2);
-}
-
-void I3(){
-  onButtonPress(3);
-}
-
-void I4(){
-  onButtonPress(4);
-}
-
-void onButtonPress(int b) {
-  // check if going high to low
-  // ignore future changes for next 0.5 seconds
-    if ((millis() - timeAtInterrupt) > 50){
-      timeAtInterrupt = millis();
-      if (b == 2) {
-        buttonflag2 = true;
-      }
-      if (b == 3) {
-        buttonflag3 = true;
-      }
-      if (b == 4) {
-        buttonflag4 = true;
-      }
-
-      counter++;
-    } 
 }
 
 
 void loop() {
   int button1Mode = digitalRead(2);
 //  Serial.print();
-  if (buttonflag2) {
+  if (digitalRead(2)==1) {
     Serial.println("Button 1 press");
-    buttonflag2 = false;
+    // tone goes here
   }
-    if (buttonflag3) {
+    if (digitalRead(3)==1) {
     Serial.println("Button 2 press");
-    buttonflag3 = false;
+    // tone
   }
-    if (buttonflag4) {
+    if (digitalRead(4)==1) {
     Serial.println("Button 3 press");
-    buttonflag4 = false;
+    // tone
   }
   
   
