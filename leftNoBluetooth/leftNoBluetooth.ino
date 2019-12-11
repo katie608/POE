502fa019b65c0facf8398530051955fa4f2e58e5
Katherine Foster <kfoster@olin.edu>
2019-12-11 09:41:33 -0500

    Add code for non-bluetooth

---
+++ leftNoBluetooth/leftNoBluetooth.ino
@@ -0,0 +1,67 @@
+
+
+// wire is the library that allows communication with the accelerometer using I2C
+#include <Wire.h> 
+
+// needed for communicating with this particular accelerometer
+#include "SparkFun_MMA8452Q.h" 
+
+// create instance of the MMA8452 class for accelerometer
+MMA8452Q accel;  
+
+void setup() {
+  Serial.begin(9600);
+
+  Wire.begin();
+
+//  if (accel.begin() == false) {
+//    Serial.println("Not Connected. Please check connections and read the hookup guide.");
+//    while (1);
+//  }
+
+  Serial.println("Setup Done");
+  
+}
+
+void loop() {
+
+
+  // code for force sensitive resistor
+  Serial.print(analogRead(A0));
+  Serial.print(" ");
+  Serial.print(analogRead(A1));
+  Serial.print(" ");
+  Serial.print(analogRead(A4));
+  Serial.print(" ");
+  Serial.print(analogRead(A5));
+  Serial.println(" ");
+  
+  // flex sensor code
+  Serial.print(analogRead(A2));
+  Serial.print(" ");
+  Serial.print(analogRead(A3));
+  Serial.print(" ");
+
+
+   
+
+
+
+  // accelerometer code
+//  if (accel.available()) {  // Wait for new data from accelerometer
+//    // Acceleration of x, y, and z directions in g units (between 0 and 1)
+//    float ax = (accel.getCalculatedX());
+//    float ay = (accel.getCalculatedY());
+//    float az = (accel.getCalculatedZ());
+//
+//    
+//    Serial.print(ax);
+//    Serial.print(" ");
+//    Serial.print(ay);
+//    Serial.print(" ");
+//    Serial.println(az);
+//
+//
+//  }
+
+}
--- rightNoBluetooth/rightNoBluetooth.ino
+++ rightNoBluetooth/rightNoBluetooth.ino
@@ -25,22 +25,25 @@ void setup() {
 
 void loop() {
 
+  // button
+  Serial.print(digitalRead(2));
+  Serial.print(" ");
+  Serial.print(digitalRead(3));
+  Serial.print(" ");
+  Serial.print(digitalRead(4));
+  Serial.print(" ");
+  Serial.print(digitalRead(5));
+  Serial.print(" ");
+
   // flex sensor code
   Serial.print(analogRead(A0));
   Serial.print(" ");
   Serial.print(analogRead(A1));
   Serial.print(" ");
-  Serial.print(analogRead(A2));
-  Serial.print(" ");
-  Serial.print(analogRead(A3));
-  Serial.print(" ");
-  Serial.print(analogRead(A4));
-  Serial.print(" ");
-  Serial.print(analogRead(A5));
-  Serial.println(" ");
+
+  
 
 
-    // code for force sensitive resistor
 
 
 
