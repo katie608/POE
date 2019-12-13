7a1b4f30172fb2b4b67f21435dd4b847443ec7f3
Katherine Foster <kfoster@olin.edu>
2019-12-13 13:11:00 -0500

    Couple of changes to website, and check if github can store solidworks files

--- rightHand/rightHand.ino
+++ rightHand/rightHand.ino
@@ -1,4 +1,4 @@
-// prints bluetooth data to 
+// prints data over Bluetooth from right hand
 
 #include <SoftwareSerial.h>
 
@@ -32,30 +32,16 @@ void setup() {
 }
 
 void loop() {
-//  Serial.println("Printing over Serial"   );
-//  mySerial.println("Printing over bluetooth");
-
   // accelerometer code
-//  if (accel.available()) {  // Wait for new data from accelerometer
     // Acceleration of x, y, and z directions in g units (between 0 and 1)
     float ax = (accel.getCalculatedX());
     float ay = (accel.getCalculatedY());
     float az = (accel.getCalculatedZ());
-
-    
     mySerial.print(ax);
     mySerial.print(" ");
     mySerial.print(ay);
     mySerial.print(" ");
     mySerial.println(az);
-//    Serial.print(ax);
-//    Serial.print(" ");
-//    Serial.print(ay);
-//    Serial.print(" ");
-//    Serial.println(az);  
-
-
-//}
 
   // button code
   // button 1
@@ -67,23 +53,14 @@ void loop() {
   // button 3
   mySerial.print(digitalRead(4));
   mySerial.print(" ");
-    // button 4
+  // button 4
   mySerial.print(digitalRead(5));
   mySerial.print(" ");
-  
-
-  // code for force sensitive resistor
-//  mySerial.print(analogRead(A0));
-//  mySerial.print(" ");
-
 
   // flex sensor code
   mySerial.print(analogRead(A0));
   mySerial.print(" ");
   mySerial.print(analogRead(A1));
   mySerial.print(" ");
-
-
-
 
 }
